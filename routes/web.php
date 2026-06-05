af07be5be3e993b6e5212944b8d5cba511538e60
qtvel <alyaqistinanadia@gmail.com>
2026-06-05 22:02:41 +0800

    Fix route conflict and sync SuciTrack routes

--- routes/web.php
+++ routes/web.php
@@ -4,31 +4,35 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PrayerController;
 use App\Http\Controllers\MenstrualController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
 */
 
 Route::get('/', function () {
     return redirect()->route('login');
 });
 
-
+/*
+| Dashboard
+*/
 Route::get('/dashboard', [PrayerController::class, 'index'])
     ->middleware(['auth'])
     ->name('dashboard');
-=======
-Route::middleware(['auth'])->group(function () {
-});
 
-Route::resource('menstrual_records', MenstrualController::class);
->>>>>>> hanees/main
+/*
+| Authenticated routes
+*/
+Route::middleware(['auth'])->group(function () {
+
+    // Profile routes (if used)
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Menstrual module (keep inside auth for safety)
+    Route::resource('menstrual_records', MenstrualController::class);
+});
 
